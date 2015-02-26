95181bbd4828ec46281f093fb339a3f06e249b20
arun <arunrv2011@gmail.com>
2015-02-27 01:57:24 +0530

    p09-13-runlength.ml

--- p09-13-runlength.ml
+++ p09-13-runlength.ml
@@ -12,7 +12,9 @@ let pack xs = pack' [] xs
 (* p10 - returns the runlength encoding of a list *)
 let runlength_encode xs = 
     let xs' = pack xs in
-    List.map xs' (fun (y::ys')  -> (1 + List.length ys', y)) 
+    List.map xs' (fun ys  -> match ys with 
+                    | [] -> raise (Invalid_argument "Not expecting a null list")
+                    | y::ys' -> (List.length ys, y)) 
 
 type 'a rle = 
     | One of 'a
@@ -22,5 +24,17 @@ type 'a rle =
 let runlength_encode2 xs =
     let xs' = pack xs in
     List.map xs' (fun ys -> match ys with
+                    | [] -> raise (Invalid_argument "Not expecting a null list")
                     | y::[] -> One y
                     | y::ys' -> Many (1 + List.length ys', y))
+
+(* p12 - decodes a runlength encoding as produced in p11 *)
+let rec replicate n x = match (n,x) with
+    | (0, _) -> []
+    | _ -> x::(replicate (n - 1) x)
+
+let unpack_rle rle = match rle with
+    | One x -> [x]
+    | Many (n, x) -> replicate n x
+
+let runlength_decode rles = List.concat (List.map rles unpack_rle)
