6aeab6b918e4e3841aa67baa25d1bf7cdfb699b3
arun <arunrv2011@gmail.com>
2015-02-27 02:10:27 +0530

    p14-duplicate every element of a list

--- p14-15-duplicate.ml
+++ p14-15-duplicate.ml
@@ -4,3 +4,6 @@ let rec replicate' n x = match (n, x) with
    | (n, _) -> x::(replicate' (n - 1) x)
 
 let replicate n xs =  List.concat (List.map xs (fun x -> replicate' n x))
+
+(*p14 - duplicates every element of a list*)
+let duplicate xs = replicate 2 xs
