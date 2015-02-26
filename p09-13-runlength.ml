cbd997cbb3f27db58092e48571439256433201b1
arun <arunrv2011@gmail.com>
2015-02-27 01:45:52 +0530

    p10 - runlength version 2

--- p09-13-runlength.ml
+++ p09-13-runlength.ml
@@ -10,6 +10,17 @@ let rec pack' ys xs = match ys, xs with
 let pack xs = pack' [] xs 
 
 (* p10 - returns the runlength encoding of a list *)
-let rec runlength_encode xs = 
+let runlength_encode xs = 
     let xs' = pack xs in
-    List.map xs' (fun (y::ys')  -> (y, 1 + List.length ys')) 
+    List.map xs' (fun (y::ys')  -> (1 + List.length ys', y)) 
+
+type 'a rle = 
+    | One of 'a
+    | Many of int * 'a
+
+(* p11 - runlength version 2. Returns items of type rle. *)
+let runlength_encode2 xs =
+    let xs' = pack xs in
+    List.map xs' (fun ys -> match ys with
+                    | y::[] -> One y
+                    | y::ys' -> Many (1 + List.length ys', y))
