698b83c6409d29c9c5f6abd2818b42b2c6bd6fe5
arun <arunrv2011@gmail.com>
2015-02-27 02:09:19 +0530

    p15 - replicate elements of a list n times

---
+++ p14-15-duplicate.ml
@@ -0,0 +1,6 @@
+(*p15 - replicates every element of a list n times *)
+let rec replicate' n x = match (n, x) with
+   | (0, _) -> []
+   | (n, _) -> x::(replicate' (n - 1) x)
+
+let replicate n xs =  List.concat (List.map xs (fun x -> replicate' n x))
