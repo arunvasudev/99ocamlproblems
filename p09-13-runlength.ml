8af4b913a952079df19a5bd628bd435f28b81e20
arun <arunrv2011@gmail.com>
2015-02-27 01:40:59 +0530

    p10 - runlength encoding of a list

--- p09-13-runlength.ml
+++ p09-13-runlength.ml
@@ -2,9 +2,14 @@
 let rec pack' ys xs = match ys, xs with 
  | (_,[]) -> [ys]
  | ([], (x::xs')) -> pack' [x] xs'
- | ((y::ys'), (x::xs')) -> 
+ | (y::ys', x::xs') -> 
          if (x = y) 
          then (pack' (x::ys) xs') 
          else ys::(pack' [x] xs')
 
 let pack xs = pack' [] xs 
+
+(* p10 - returns the runlength encoding of a list *)
+let rec runlength_encode xs = 
+    let xs' = pack xs in
+    List.map xs' (fun (y::ys')  -> (y, 1 + List.length ys')) 
