df6c20925f4c773ef66cadd69c9c0041e1a5f44c
arun <arunrv2011@gmail.com>
2015-02-28 20:54:46 +0530

    p20 - remove the kth element of a list

--- p20-remove-at.ml
+++ p20-remove-at.ml
@@ -1,2 +1,2 @@
 (* p20 - remove the kth element of a list *)
-let remove_at xs k = List.append (List.take (List.take xs k) (k-1)) (List.drop xs k)
+let remove_at xs k = List.append (List.take (List.take xs (k+1)) (k)) (List.drop xs (k+1))
